64a9707ca9160143e773803f63f81d590653522e
leppkjs <leppkjs@gmail.com>
2022-02-08 21:47:14 +0900

    상품 도메인 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,5 +28,19 @@ public abstract class Item {
 
     @ManyToMany(fetch = FetchType.LAZY, mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        if(restStock < 0) {
+            throw  new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+    }
 
 }
